14364613b1eb73f8e1a741095396aade87e0f1e5
Bom <11377ny@gmail.com>
2022-07-15 16:24:34 -0400

    Arrange data

--- journal_des_vin/serializers.py
+++ journal_des_vin/serializers.py
@@ -1,26 +1,34 @@
+from django.http import QueryDict
 from rest_framework import serializers
 from .models import Wine, Winemaker
 
 
-class WinemakerSerializer(serializers.HyperlinkedModelSerializer):
-    wines = serializers.HyperlinkedRelatedField(
-        view_name='wine_detail',
-        many=True,
-        read_only=True
-    )
+class WinemakerSerializer(serializers.ModelSerializer):
+    # wines = serializers.HyperlinkedRelatedField(
+    #     view_name='wine_detail',
+    #     many=True,
+    #     read_only=True
+    # )
 
     class Meta:
+        depth = 1
         model = Winemaker
         fields = ('id', 'name', 'location', 'description', 'wines')
 
 
 class WineSerializer(serializers.HyperlinkedModelSerializer):
-    winemaker = serializers.HyperlinkedRelatedField(
-        view_name='winemaker_detail',
-        read_only=True
+    # winemaker = serializers.HyperlinkedRelatedField(
+    #     view_name='winemaker_detail',
+    #     read_only=True
+    # )
+    winemaker = WinemakerSerializer()
+    winemaker_id = serializers.PrimaryKeyRelatedField(
+        queryset = Winemaker.objects.all(),
+        source = 'winemaker'
     )
 
     class Meta:
+        depth = 1
         model = Wine
         fields = ('id', 'name', 'type', 'grape', 'country',
-                 'price', 'label', 'link', 'winemaker')
+                 'price', 'label', 'link', 'winemaker_id')
