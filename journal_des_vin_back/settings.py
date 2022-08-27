f8482ef23656cc199a1c864d87186c7e61e65848
Bom <11377ny@gmail.com>
2022-08-27 01:55:43 -0400

    Add django-cors-headers

--- journal_des_vin_back/settings.py
+++ journal_des_vin_back/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'journal_des_vin',
     'django_extensions',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'journal_des_vin_back.urls'
 
